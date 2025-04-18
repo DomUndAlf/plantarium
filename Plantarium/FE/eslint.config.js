322ce9303fdc2da09796bad9cba54d7aa80d89fa
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-04-18 14:48:10 +0200

    setup|ESLint

--- Plantarium/FE/eslint.config.js
+++ Plantarium/FE/eslint.config.js
@@ -1,28 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      camelcase: ["error", { properties: "always" }],
     },
   },
-)
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
+    languageOptions: {
+       globals: {
+        ...globals.browser, 
+        ...globals.node} 
+      } 
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {  
+    files: ["**/*.json"], 
+    plugins: { json }, 
+    language: "json/json", 
+    extends: ["json/recommended"] 
+  },
+  { 
+    files: ["**/*.css"], 
+    plugins: { css }, 
+    language: "css/css", 
+    extends: ["css/recommended"] },
+]);
