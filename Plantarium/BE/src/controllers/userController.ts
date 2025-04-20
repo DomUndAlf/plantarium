6a20c279459bd82db4fdbea4f8ddce94d84b61b9
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-04-20 07:14:15 +0200

    getAllUsers angepasst

--- Plantarium/BE/src/controllers/userController.ts
+++ Plantarium/BE/src/controllers/userController.ts
@@ -3,14 +3,11 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const userClient = prisma.users;
 
-export const getAllUsers = async (req: any, res: any) => {
-  const allUsers = await userClient.findMany({
-    include: {
-      beds: true,
-    },
-  });
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getAllUsers = async (req:any, res:any) => {
+  const allUsers = await userClient.findMany();
   res.status(200).json({ data: allUsers });
 };
 
-export default getAllUsers;
+
+
