8da2f31dbe3ef8032f07b33f1a1f0af7e4f66065
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-05-09 09:56:09 +0200

    fix: umleitung auf /dev1/

--- Plantarium/FE/src/main.tsx
+++ Plantarium/FE/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (window.location.pathname === "/dev1") {
+  window.location.replace("/dev1/");
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
