80391ff7482b4a6ea372847f748eda4ee7da2390
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-05-12 08:52:34 +0200

    test: relative path in config

--- Plantarium/FE/vite.config.ts
+++ Plantarium/FE/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
   return {
-    base: '/dev1/',
+    base: './',
     plugins: [react()],
     server: {
         host: '0.0.0.0',
