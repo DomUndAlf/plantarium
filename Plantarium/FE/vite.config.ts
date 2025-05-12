e43e59cf8baac23c1e037402f3bb02cdd6301198
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-05-12 09:14:05 +0200

    test: base /dev in config

--- Plantarium/FE/vite.config.ts
+++ Plantarium/FE/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/dev1',
     plugins: [react()],
     server: {
         host: '0.0.0.0',
