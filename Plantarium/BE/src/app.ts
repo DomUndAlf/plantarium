bff27656bb2a6379c68393626a981e28297f2135
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-05-18 18:37:39 +0200

    oauth|password,jwt,dockerfiles angepasst, docker compose angepasst, prod compose angelegt, scripts zum lok dev angepasst,

--- Plantarium/BE/src/app.ts
+++ Plantarium/BE/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import userRouter from './routes/user.routes';
 import cors from 'cors';
+import passport from 'passport';
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+app.use(passport.initialize());
 
 app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
   res.send('Hello, Worlddd!');
