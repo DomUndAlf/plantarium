0fb635d6916e13535c33a29e431c09d095629ad2
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-05-28 22:14:11 +0200

    createGarden | create Garden wird nur aufgerufen wenn Nutzer wirklich neu ist

--- Plantarium/FE/src/components/Dashboard.tsx
+++ Plantarium/FE/src/components/Dashboard.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import CreateGarden from "./CreateGarden";
+import Dash from "./Dash";
 
 function Dashboard () {
-    return (
-        
-        <>
-        <CreateGarden />
-        </>
-    );
+    const [user, setUser] = useState<any>(null);
+
+    useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch("http://localhost:3001/users/me", {
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setUser(data.data); // ⬅️ Struktur wie in deinem Backend
+      }
+    };
+     fetchUser();
+  }, []);
+
+ function isNew():boolean {
+    if (!user)
+        return true;
+    else return false;
+    }
+    
+    if (isNew()) return <CreateGarden/>
+    else 
+    return (<p> Hallo {user.name} </p>);
 }
 
 export default Dashboard;
