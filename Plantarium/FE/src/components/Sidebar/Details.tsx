1cfd54bef4e1a0f92730ddda161db64f4dd3734e
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-07-30 22:21:09 +0200

    createGarden | gartengröße in sidebar editierbar

--- Plantarium/FE/src/components/Sidebar/Details.tsx
+++ Plantarium/FE/src/components/Sidebar/Details.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../mainStructure/MainFrame";
-import { Button } from "@headlessui/react";
+import { Button, Input } from "@headlessui/react";
+import { IGarden } from "../../interfaces/interfaces";
 
 type Props = {
     detailName: string;
@@ -8,18 +9,95 @@ type Props = {
 
 function Details({ detailName }: Props) {
     const user = useContext(UserContext);
+
+    const [garden, setGarden] = useState<IGarden>({
+        name: "",
+        location: "",
+        height: "",
+        width: "",
+    });
+
+    useEffect(() => {
+        if (user) {
+            setGarden({
+                name: user.name || "",
+                location: user.location || "",
+                height: user.height || "",
+                width: user.width || "",
+            });
+        }
+    }, [user]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setGarden({
+            ...garden,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const res = await fetch("http://localhost:3001/users/me/garden", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                name: garden.name,
+                location: garden.location,
+                height: Number(garden.height),
+                width: Number(garden.width),
+            }),
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Update erfolgreich:", data);
+            window.location.reload();
+        } else {
+            console.error("Fehler beim Update");
+        }
+    };
+
     switch (detailName) {
         case "garden":
             return (
-                <div className="flex items-center flex-col">
-            <div className="w-90 p-5 bg-white/80 text-black rounded-xl mb-3">
-                <p className="font-semibold">Ort:</p> <p className="font-light">{user?.location}</p>
-                <p className="font-semibold">Höhe:</p> <p className="font-light">{user?.height} m</p>
-                <p className="font-semibold">Breite:</p> <p className="font-light">{user?.width} m </p>
-            </div>
-            <Button className="m-3 p-2 pl-8 pr-8 rounded-xl bg-darkMint/80 font-normal hover:bg-darkMint/50 active:scale-97 transition duration-150">
-                        Edit </Button>
-            </div>);
+                <form onSubmit={handleSubmit} className="flex items-center flex-col">
+                    <div className="w-90 p-5 bg-white/80 text-black rounded-xl mb-3">
+                        <p className="font-semibold">Ort:</p>
+                        <Input
+                            className="font-light w-80"
+                            name="location"
+                            value={garden.location}
+                            onChange={handleChange}
+                        />
+                        <p className="font-semibold">Höhe:</p>
+                        <Input
+                            className="font-light"
+                            name="height"
+                            value={garden.height}
+                            onChange={handleChange}
+                        />
+                        <p className="font-semibold">Breite:</p>
+                        <Input
+                            className="font-light"
+                            name="width"
+                            value={garden.width}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        className="m-3 p-2 pl-8 pr-8 rounded-xl bg-darkMint/80 font-normal hover:bg-darkMint/50 active:scale-97 transition duration-150"
+                    >
+                        Update
+                    </Button>
+                </form>
+            );
+
+
         case "structure":
             // hier gehört noch mapping hin
             return (
