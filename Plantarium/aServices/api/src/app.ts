310ee0afb419c0932e10afc0c9e7b030c0a3e1ec
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-08-28 17:48:02 +0200

    beide app.ts anpassen

--- Plantarium/aServices/api/src/app.ts
+++ Plantarium/aServices/api/src/app.ts
@@ -1,29 +1,23 @@
-import express from 'express';
-import userRouter from './routes/user.routes';
-import cors from 'cors';
-import passport from 'passport';
-import cookieParser from 'cookie-parser';
-import surfaceRouter from '../../../BE/src/routes/structBeds.routes';
-import bedRouter from '../../../BE/src/routes/bed.routes';
-import bedPlantsRouter from '../../../BE/src/routes/bedplants.routes';
-import individualPlantsRouter from '../../../BE/src/routes/plants.router';
-const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import surfaceRouter from "./routes/structBeds.routes";
+import bedRouter from "./routes/bed.routes";
+import bedPlantsRouter from "./routes/bedplants.routes";
+import individualPlantsRouter from "./routes/plants.router";
 
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(passport.initialize());
 
-app.use('/users', userRouter);
-app.use('/me/garden/surfaces', surfaceRouter);
-app.use('/me/garden/individual-plants', individualPlantsRouter);
+app.get("/health", (_req, res) => res.json({ ok: true, service: "api" }));
+
+// exakt deine Pfade
+app.use("/me/garden/surfaces", surfaceRouter);
+app.use("/me/garden/individual-plants", individualPlantsRouter);
 app.use("/me/garden/beds", bedPlantsRouter);
-app.use('/me/garden/beds', bedRouter);
+app.use("/me/garden/beds", bedRouter);
 
 export default app;
-
-//middleware
-//routen definieren
