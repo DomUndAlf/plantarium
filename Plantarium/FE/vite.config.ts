744e2f3bd537372bfc054c459d35b391051756cb
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-09-11 18:14:57 +0200

    vite config

--- Plantarium/FE/vite.config.ts
+++ Plantarium/FE/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/dev14/',
     plugins: [react(), tailwindcss(),],
     server: {
         host: '0.0.0.0',
