760dbe63e5e5aa871259cfee51000a146e56c908
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-09-11 13:43:48 +0200

    experiment pfad

--- Plantarium/aServices/struct/src/routes/structBeds.routes.ts
+++ Plantarium/aServices/struct/src/routes/structBeds.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import prisma from "../../../shared/prismaClient";
+import prisma from "../../shared/prismaClient";
 import jwt from "jsonwebtoken";
 
 const surfaceRouter = Router();
