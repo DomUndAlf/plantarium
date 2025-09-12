a1a221fae2cb798808a7fd983378a781667febe8
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-09-12 16:42:43 +0200

    bild + no cache

--- Plantarium/FE/src/components/firstLogin/Welcome.tsx
+++ Plantarium/FE/src/components/firstLogin/Welcome.tsx
@@ -10,7 +10,7 @@ export default function Welcome() {
   return (
 <div className="flex flex-col items-center justify-center min-h-screen space-y-6 p-4">
       <p className="text-lg text-mint font-medium">Willkommen bei deiner Gartenapp</p>
-      <img src={fontLogo} alt="Logo" className="w-[80vw] h-auto" />
+    <h1> PLANTARIUM </h1>
       <Button
         className="bg-mint font-medium rounded-xl w-40 mt-7 p-2 text-white"
         onClick={handleLogin}
