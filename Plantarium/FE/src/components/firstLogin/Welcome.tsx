17cafa725542549f1dedb164501954620f1c5dce
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2025-09-12 16:33:29 +0200

    bild fixen

--- Plantarium/FE/src/components/firstLogin/Welcome.tsx
+++ Plantarium/FE/src/components/firstLogin/Welcome.tsx
@@ -1,4 +1,6 @@
+
 import { Button } from "@headlessui/react";
+import fontLogo from "/assets/font.png";
 
 export default function Welcome() {
   const handleLogin = () => {
@@ -8,7 +10,7 @@ export default function Welcome() {
   return (
 <div className="flex flex-col items-center justify-center min-h-screen space-y-6 p-4">
       <p className="text-lg text-mint font-medium">Willkommen bei deiner Gartenapp</p>
-      <img src="./assets/font.png" alt="Logo" className="w-[80vw] h-auto" />
+      <img src={fontLogo} alt="Logo" className="w-[80vw] h-auto" />
       <Button
         className="bg-mint font-medium rounded-xl w-40 mt-7 p-2 text-white"
         onClick={handleLogin}
